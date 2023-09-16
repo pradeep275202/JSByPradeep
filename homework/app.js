f687015b2e8bbb4b73a1df410d52e8cf97abdaac
pradeep <pr275202@gmail.com>
2023-09-16 21:46:28 +0530

    16_sep_allwork

--- homework/app.js
+++ homework/app.js
@@ -167,7 +167,7 @@ var comparison3 = comparison == comparison2;
 console.log('comparison3 :', comparison3)
 
 
-//    - Not equal to !=
+// ***** Not equal to != *****
 var comparison4 = comparison != comparison2;
 console.log('comparison4 :', comparison4)
 
@@ -183,6 +183,130 @@ console.log("compa2",compa2)
 // ---- not equal to != ----
 let compa3 = compa != compa1
 console.log("compa3",compa3)
+
+// *****strict equal to *****
+// -----first exmple ----
+
+let strict = 80;
+let strict1 = 80;
+let strict3 = strict === strict1
+console.log(" value is strict3 = ",strict3)
+
+// ---- second example ----
+
+let se = 20;
+let se1 = 30;
+let se3 = se === se1
+console.log("value is se3 = ",se3)
+
+// ---- third example ----
+
+let th = 70;
+let th1 = 40;
+let th2 = th === th1
+console.log("value of th2 = ", th2)
+
+// **** strict not equal ****
+//---- first example ----
+
+let notstrict = 40;
+let notstrict1 = 50;
+let notstrict3 = notstrict !== notstrict1
+console.log("not strict value is = ",notstrict3 )
+
+// ---- second example ----
+
+let ns = 90;
+let ns1 = 90;
+let ns2 = ns !==  ns1
+console.log("value of ns2 = ", ns2)
+
+// ---- third example -----
+
+let thi = 50 ;
+let thi1 = 40 ;
+let thi2 = thi !== thi1
+console.log("value of thi2 = ", thi2)
+
+//    ***** Greater than > ******
+// ---- first example ----
+
+let greater = 50;
+let greater1 = 60;
+let greater2 = greater1 > greater
+console.log(" value of greater1 > greater = ", greater2)
+let greater3 = greater > greater1
+console.log("value of greater1 > greater =", greater3)
+
+// ----- second example ----
+
+let great = 30;
+let great1 = 20;
+let great2 = great > great1
+console.log("value of great > great1 = ", great2)
+let great3 = great1 > great
+console.log("value of great1 > great = ", great3)
+
+//   ***** - Less than < *****
+// -----first example -----
+
+let less = 40;
+let less1 = 41;
+let less2 = less < less1
+console.log("value of less > less 1 = ",less2)
+let less3 = less1 > less 
+console.log("value of less1 > less = ",less3)
+
+// ----- SECOND EXAMPLE -----
+
+let les = 50;
+let les1 = 20;
+let les2 = les1 < les 
+console.log("value of les1 < les ", les2)
+let les3 = les < les1
+console.log("value of les > les1 ", les3)
+
+//   ----Greater than or equal to >= ----
+//   ---- first example ----
+
+let equal = 100;
+let equal1 = 50;
+let equal2 = equal >= equal1
+console .log("value of equal >= equal1 = ", equal2)
+let equal3 = equal1 >= equal
+console.log("value of equal1 > equal = ", equal3)
+
+// ---- second example ----
+
+let eq = 50;
+let eq1 = 60;
+let eq2 = eq1 >= eq
+console.log("value of eq1 > eq = ", eq2)
+let eq3 = eq >= eq1
+console.log("let eq >= eq1 = ", eq3)
+
+//  ---- Less than or equal to <= ----
+// ----- first example ----
+
+let eqa = 40;
+let eqa1 = 50;
+let eqa2 = eqa1 <= eqa
+console.log("value of eqa <= eqa1 =", eqa2)
+let eqa3 = eqa1 <= eqa
+console.log("value of eqa3 = ", eqa3)
+
+// ----- second example ----
+
+let equ = 100;
+let equ1 = 100;
+let equ2 = equ <= equ1
+console.log("value of equ2 = ", equ2)
+let equ3 = equ1 <= equ
+console.log("value of equ3 = ",equ3) 
+
+
+
+
 
 
 
