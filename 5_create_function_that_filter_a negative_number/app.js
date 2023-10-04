cf861723bd0eca2035f64676bcb33027057f1d53
pradeep <pr275202@gmail.com>
2023-10-04 22:59:57 +0530

    pradeep_allwork_4_oct

--- 5_create_function_that_filter_a negative_number/app.js
+++ 5_create_function_that_filter_a negative_number/app.js
@@ -5,7 +5,7 @@ function array() {
                     
                     
           }
-}*/
+}
 
 let array = [1,-1,2,-2,3,-3,4,-4,5,-5,6,-6,]
 var filter =[]
@@ -13,4 +13,17 @@ for (var i= 0; i <=0; i++) {
           filter.push(array[i])
           console.log(filter)
           
-}
+}*/
+
+function reverce() {
+          
+
+var a = [1,2,-3,4,-5,6,-7,8,-9]
+var newarray = []
+for(let i=a.length;i>0;i++)
+     if(i<=0){
+          newarray.push(i[a])
+          console.log(i)
+     }
+}  
+reverce()
