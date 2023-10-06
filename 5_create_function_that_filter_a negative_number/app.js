f279e3c034db1401e1889d2a437a103b2a3ea4e2
pradeep <pr275202@gmail.com>
2023-10-06 22:46:08 +0530

    6_oct_pradeepallwork

--- 5_create_function_that_filter_a negative_number/app.js
+++ 5_create_function_that_filter_a negative_number/app.js
@@ -13,15 +13,40 @@ for (var i= 0; i <=0; i++) {
           filter.push(array[i])
           console.log(filter)
           
-}*/
+}
 
 function filter() {          
 var a = [1,2,-3,4,-5,6,-7,8,-9]
 var newarray = []
-for(let i=a.length;i>0;i++)
-     if(0<i){
+for(let i=0;i=a.length;i++)
+     if(a[i]<=0){
           newarray.push(i[a])
-          console.log(newarray)
+          
      }
+     console.log(newarray)
 }  
 filter()
+function filter() {          
+     var a = [1,2,-3,4,-5,6,-7,8,-9]
+     var newarray = []
+     for(let i=0;i=a.length;i++)
+          if(a[i]<=0){
+               newarray.push(i[a])
+               
+          }
+          console.log(newarray)
+     }  
+     filter()*/
+     
+     function filternumber(){
+          var array = [1,2,-3,4,-5,-6,7,-8,-9]
+          var newarray = []
+          for(i=0;i<array.length;i++)
+          if(array[i]<=0){
+               newarray.push(array[i])
+          }
+          console.log(newarray)
+
+          
+     }
+     filternumber()
---
+++ practice/break continou nested loop/app.js
@@ -0,0 +1,13 @@
+for(i=0;i<=100;i++){
+   document.write('aashish')
+   document.write('br')
+   for(i=0;i<=5;i++){
+          document.write('kumar')
+          document.write('br')
+          if(i%2 ==0){
+                    document.write('kumar')
+                    document.write('br')
+                    break;
+          }
+   }  
+}
