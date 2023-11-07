18d962949865add4169445d6d9c69caa318ae7e4
pradeep <pr275202@gmail.com>
2023-11-07 22:17:38 +0530

    7_pradeep_all_work

---
+++ practice/all/function/app.js
@@ -0,0 +1,22 @@
+// use for loop in function and print 1 to 100 number
+function number() {
+          for(i=0;i<100;i++)
+          console.log(i)
+          
+}
+number()
+
+// print 100 to 1 number
+function numbers() {
+          for(i=100;i>0;i--)
+          console.log(i)         
+}
+numbers()
+// print sum numbers in function
+function sum(){
+          for(i=0;i<100;i++)
+          if (i%2==0) {
+                    console.log(i)                    
+          }
+}
+sum()
