33b335783926f262b5269758561a0ef33e0ffa8f
pradeep <pr275202@gmail.com>
2023-11-24 22:00:52 +0530

    24_nov_pradeep_allwork

--- all_question/6_create_the_sum_first_hundred_prime_number/app.js
+++ all_question/6_create_the_sum_first_hundred_prime_number/app.js
@@ -1,6 +1,45 @@
-// create the first hundred prime number
-for (var i = 1; i <= 100; i++) {
-  if (i % 2 == 1 && i % 3 == 1 && i % 5 == 1) {
-    console.log(i);
+// function isPrime(n) {
+//   if (n <= 1) {
+//     return false;
+//   }
+//   for (var i = 2; i < n; i++) {
+//     console.log('i value', i,"n value ",n," n % i == 0 ",n%i)
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isPrime(5)); // false
+// // console.log(isPrime(11)); // true
+
+
+// function primenumber(k){
+//   var prime =[];
+//   for(var i=1;i<k;i++){
+//     if(k % 2=== 0){
+//       return false;
+//     }
+
+//   }
+//   return true;
+
+// }
+// console.log(primenumber(100))
+
+function primenumber(k){
+  for(var i = 2; i<k; i++){
+      if(k % i === 0){
+          return false;
+          
+        
+      }
+  }
+  return true;  
+}
+for(i=0;i<=100;i++){
+  if(primenumber(i)){
+      console.log(i)
   }
 }
