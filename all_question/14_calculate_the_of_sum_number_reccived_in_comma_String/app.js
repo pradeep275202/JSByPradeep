b4f2f3e8bba5a44b6854d67f98de7312497e820d
pradeep <pr275202@gmail.com>
2023-12-08 20:55:18 +0530

    8_solving_question

--- all_question/14_calculate_the_of_sum_number_reccived_in_comma_String/app.js
+++ all_question/14_calculate_the_of_sum_number_reccived_in_comma_String/app.js
@@ -1,5 +1,10 @@
-let string = "1, 2, 4, 6, 8"
-let addingcommastring = "  "
-for(var i=0;i<string.length+1;i++){
-          console.log(i)
+var string ="1,2,4,6,8"
+var replace = string.replaceAll(",","");
+//console.log(replace)
+var addingcommastring = "";
+for(var i=0; i<replace.length; i++){
+          console.log("------",string[i])
+          addingcommastring += string[i]
+          console.log(addingcommastring)
+          
 }
