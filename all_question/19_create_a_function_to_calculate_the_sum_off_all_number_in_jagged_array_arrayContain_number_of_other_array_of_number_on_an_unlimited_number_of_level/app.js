4f4cfc271aa8edc8011898f55547989b5db95ffb
pradeep <pr275202@gmail.com>
2023-12-18 20:52:29 +0530

    18_dec_pradeep_allwork

--- all_question/19_create_a_function_to_calculate_the_sum_off_all_number_in_jagged_array_arrayContain_number_of_other_array_of_number_on_an_unlimited_number_of_level/app.js
+++ all_question/19_create_a_function_to_calculate_the_sum_off_all_number_in_jagged_array_arrayContain_number_of_other_array_of_number_on_an_unlimited_number_of_level/app.js
@@ -1,8 +1,8 @@
 function jagged(){
          var jaggedArray = [1,2,3,4,[2,34,[2,432,[44,4,55,33],4234,34],656,44,],1,24,,6,8,8];
-         var array = jaggedArray.flat()
+         var array =Array.isArray(jagged)
           var sum = 0;
-          for (var i = 0; i <= array.length+1; i++) {
+          for (var i = 0; i < array.length; i++) {
                     //console.log("this is jagged index value ===",i)
             for (var j = 0; j <= array[i].length; j++) {
                     //console.log(jaggedArray[i][j])     ;
