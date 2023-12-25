ca6622b0974a1c9dba8ded4b163620c0a470ad25
pradeep <pr275202@gmail.com>
2023-12-25 21:44:06 +0530

    25_dec_pradeep_allwork

--- all_question/8_find_maximum_number _in_array_number/app.js
+++ all_question/8_find_maximum_number _in_array_number/app.js
@@ -1,5 +1,5 @@
 var array = [1,2,3,4,5,6,7,8,9]
-var max = [0];
+var max = 0;
 for(var i = 0;i<array.length;i++){
           if(array[i] >= max){
                    max=(array[i])
@@ -8,12 +8,12 @@ for(var i = 0;i<array.length;i++){
           console.log(max)
 }
 
-var array = [1,2,3,4,5,6,7,8,9]
-var max = array[0];
-for(var i = 0;i<array.length;i++){
-          if(array[i]>max){
-                   max=(array[i])
+//var array = [1,2,3,4,5,6,7,8,9]
+//var max = array[0];
+//for(var i = 0;i<array.length;i++){
+//          if(array[i]>max){
+//                   max=(array[i])
                    
-          }
-          console.log(max)
-}
+ //         }
+ //         console.log(max)
+//}
